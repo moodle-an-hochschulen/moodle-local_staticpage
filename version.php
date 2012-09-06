3c0ba7be2b98fe28bee7275d57d7d0af347d85a3
Alexander Bias <github@alexanderbias.de>
2012-09-06 12:43:40 +0200

    Update version.php for correct Moodle 2.3 release date

--- version.php
+++ version.php
@@ -15,7 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 $plugin->component = 'local_staticpage';
-$plugin->version = 2012061700;
-$plugin->release = '2.3 (Build: 2012061700)';
-$plugin->requires = 2012061700;
+$plugin->version = 2012062500;
+$plugin->release = '2.3 (Build: 2012062500)';
+$plugin->requires = 2012062500;
 $plugin->maturity = MATURITY_STABLE;
