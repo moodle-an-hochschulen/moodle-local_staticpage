25657845e1729c13ece5615cd8db144bda4bc216
Alexander Bias <github@alexanderbias.de>
2013-10-29 10:35:35 +0100

    Bump version.php

--- version.php
+++ version.php
@@ -17,7 +17,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_staticpage';
-$plugin->version = 2013073001;
-$plugin->release = '2.5 (Build: 2013090200)';
+$plugin->version = 2013073002;
+$plugin->release = '2.5 (Build: 2013102900)';
 $plugin->requires = 2013051401;
 $plugin->maturity = MATURITY_STABLE;
