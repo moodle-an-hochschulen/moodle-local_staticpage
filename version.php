9d7730f8e2478068a2cf55bea40d5e004b32c51d
Alexander Bias <alexander.bias@uni-ulm.de>
2017-01-12 13:44:55 +0100

    Upgrade README.md

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_staticpage';
-$plugin->version = 2017011200;
-$plugin->release = 'v3.2-r1';
+$plugin->version = 2017011201;
+$plugin->release = 'v3.2-r2';
 $plugin->requires = 2016120500;
 $plugin->maturity = MATURITY_STABLE;
