28fc9bc671470a8a7164cdd3985bccce34e3ab27
Alexander Bias <alexander.bias@uni-ulm.de>
2017-07-04 08:01:25 +0200

    Make codechecker fully happy

--- view.php
+++ view.php
@@ -23,7 +23,11 @@
  */
 
 // Include config.php.
+// @codingStandardsIgnoreStart
+// Let codechecker ignore the next line because otherwise it would complain about a missing login check
+// after requiring config.php which is really not needed.
 require_once('../../config.php');
+// @codingStandardsIgnoreEnd
 
 // Include lib.php.
 require_once(dirname(__FILE__) . '/lib.php');
