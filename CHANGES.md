058b3572e37d230efbd4034dc7e31da16f9b6405
Alexander Bias <alexander.bias@uni-ulm.de>
2018-05-17 20:32:49 +0200

    Release v3.4-r2

--- CHANGES.md
+++ CHANGES.md
@@ -4,7 +4,7 @@ moodle-local_staticpage
 Changes
 -------
 
-### Unreleased
+### v3.4-r2
 
 * 2018-05-16 - Implement Privacy API.
 
--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_staticpage';
-$plugin->version = 2018051600;
-$plugin->release = 'v3.4-r1';
+$plugin->version = 2018051700;
+$plugin->release = 'v3.4-r2';
 $plugin->requires = 2017111300;
 $plugin->maturity = MATURITY_STABLE;
