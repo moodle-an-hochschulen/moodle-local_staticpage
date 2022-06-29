ca4cd89a010bb5648304979b0c18e96078e9ee80
Alexander Bias <bias@alexanderbias.de>
2022-06-29 21:22:23 +0200

    Release v3.11-r2

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_staticpage';
-$plugin->version = 2021120800;
-$plugin->release = 'v3.11-r1';
+$plugin->version = 2021120801;
+$plugin->release = 'v3.11-r2';
 $plugin->requires = 2021051700;
 $plugin->supported = [311, 311];
 $plugin->maturity = MATURITY_STABLE;
