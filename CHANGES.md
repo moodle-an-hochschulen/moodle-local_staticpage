5b55b78c2faecd44c39faea443a13aea86cfe8c2
Alexander Bias <bias@alexanderbias.de>
2025-05-17 18:51:55 +0200

    Release v5.0-r1

--- CHANGES.md
+++ CHANGES.md
@@ -4,7 +4,7 @@ moodle-local_staticpage
 Changes
 -------
 
-### Unreleased
+### v5.0-r1
 
 * 2025-04-14 - Prepare compatibility for Moodle 5.0.
 
--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_staticpage';
-$plugin->version = 2024100701;
-$plugin->release = 'v4.5-r2';
+$plugin->version = 2025041400;
+$plugin->release = 'v5.0-r1';
 $plugin->requires = 2025041400;
 $plugin->supported = [500, 500];
 $plugin->maturity = MATURITY_STABLE;
